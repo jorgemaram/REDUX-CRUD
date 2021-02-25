c2c0f275c37261a2828645cfdc776e0997179ac3
Jorge Martin <jorgemaram@gmail.com>
2021-02-25 12:34:01 +0100

    get products in the state

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect} from 'react';
+
+//Redux
+import { useSelector, useDispatch } from 'react-redux';
+import {obtenerProductosAction} from '../actions/productoActions';
 
 const Productos = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        //Consultar la api
+        const cargarProductos = () => dispatch(obtenerProductosAction());
+        cargarProductos();
+    }, [])
     return (  
         <>
             <h2 className='text-center my-5'>Listado de productos</h2>
