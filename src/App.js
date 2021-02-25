8bbda467934035440867b2ca6d09281b4590d6de
Jorge Martin <jorgemaram@gmail.com>
2021-02-25 12:49:14 +0100

    display products on the homepage.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Productos from "./components/Productos";
 import NuevoProducto from "./components/NuevoProducto";
-import EditarProducto from "./components/NuevoProducto";
+import EditarProducto from "./components/EditarProducto";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 //Redux
